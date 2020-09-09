d1b5d849c5cb85157cfd23170582d1c81e44a06e
s1ntaxe770r <cyberdev01@protonmail.com>
2020-09-09 11:28:21 +0100

    add more functionality :alembic:

--- containerman.py
+++ containerman.py
@@ -1,16 +1,29 @@
 import docker
 
-client =  docker.from_env()
+client = docker.from_env()
 
-class ContainerMan(object):
+
+class ContainerMan:
+
+	@classmethod
     def GetRunning():
         """retruns a list of all running containers. Return Type: list """
         running = client.containers.list()
         return running
+
+    @classmethod
     def DockerInfo():
-        """ Generic Docker Info. Retrun Type: Dict
-        pass
+        """ Generic Docker Info. Retrun Type: Dict """
+        parsed_info = {}
+        information = client.info()
+        running_containers = information["ContainersRunning"]
+        stopped_containers = information["ContainersStopped"]
+        total_images = information["Images"]
+        parsed_info["running_containers"] = running_containers
+        parsed_info["stopped_containers"] = stopped_containers
+        parsed_info["total_images"] = total_images
 
-x = ContainerMan.DockerInfo()
+        return parsed_info	
 
-print(x)
+
+
