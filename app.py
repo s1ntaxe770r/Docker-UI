e8a674abc8eb582b11af596f63c3444017ecd08f
s1ntaxe770r <cyberdev01@protonmail.com>
2020-09-30 01:54:13 +0200

    Rename Blueprint

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
-from flask import Flask ,render_template
-from Api.api import api_route
-import docker
+from flask import Flask, render_template
+from Api.api import api
 
 
-app =  Flask(__name__)
-app.register_blueprint(api_route,url_prefix='/api')
+app = Flask(__name__)
+app.register_blueprint(api, url_prefix="/api")
 
 
 @app.route("/")
 def index():
 
-	return "WIP"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return "WIP"
