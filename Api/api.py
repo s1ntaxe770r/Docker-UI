5e984d8bda114ac5bd04c7b72febcdcd6590384f
s1ntaxe770r <cyberdev01@protonmail.com>
2020-10-16 15:11:09 +0200

    create new route

--- Api/api.py
+++ Api/api.py
@@ -18,6 +18,13 @@ def running():
     json = {"containers":running_containers}
     return jsonify(json)
 
+
+@api.route("/containers/all", methods=["GET"])
+def all():
+	all_containers = cm.AllContainers()
+	json  = {"all_containers":all_containers}
+	return json
+
     
     
     
