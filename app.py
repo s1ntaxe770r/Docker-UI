9fe05a89b134f2e571e711e4681718d581037517
s1ntaxe770r <cyberdev01@protonmail.com>
2020-12-01 21:46:51 +0100

    stopped procrastinating and return actual

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 from Api.api import api
+from containerman import ContainerMan as cm 
 
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix="/api")
 
 
+# Todo: implement simple authentication 
 @app.route("/")
 def index():
+    stats = cm.DockerInfo()
+    active:str = stats["running_containers"]
+    stopped:str = stats["stopped_containers"]
+    total:str = str(cm.AllContainers())
 
-    return "WIP"
+    return render_template("index.html",active=active,stopped=stopped,total=total)
+
+
+
